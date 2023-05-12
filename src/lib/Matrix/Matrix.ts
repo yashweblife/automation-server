a9fbf738f21feea6dde8a2399646ae2dfdbd19d7
yashweblife <yashweblife20@gmail.com>
2023-05-12 01:44:26 -0500

    add matrix

--- src/lib/Matrix/Matrix.ts
+++ src/lib/Matrix/Matrix.ts
@@ -1,14 +1,48 @@
 export class Matrix{
     public components:Float32Array
-    constructor(size:number){
+    constructor(public size:number){
         this.components = new Float32Array(size);
     }
 }
 
 export class MatrixMath{
     constructor(){}
-    public add(a:Matrix, b:Matrix){}
-    public subtract(a:Matrix, b:Matrix){}
-    public magnitude(a:Matrix){}
-    public normalize(a:Matrix, b:number=1){}
+    public add(a:Matrix, b:Matrix){
+        const output = new Float32Array(a.size)
+        for(let i=0;i<a.size;i++){
+            output[i] = a.components[i]+b.components[i];
+        }
+        return(output)
+    }
+    public subtract(a:Matrix, b:Matrix){
+        const output = new Float32Array(a.size)
+        for(let i=0;i<a.size;i++){
+            output[i] = a.components[i]-b.components[i];
+        }
+        return(output)
+    }
+    public magnitude(a:Matrix){
+        let output = 0;
+        for(let i=0;i<a.size;i++){
+            output += a.components[i]**2
+        }
+        return(Math.sqrt(output))
+    }
+    public distance(a:Matrix, b:Matrix){
+        let output = 0;
+        for(let i=0;i<a.size;i++){
+            output += (a.components[i]-b.components[i])**2
+        }
+        return(Math.sqrt(output))
+    }
+    public normalize(a:Matrix, b:number=1){
+        let mag = 0;
+        for(let i=0;i<a.size;i++){
+            mag += a.components[i]**2
+        }
+        mag = b/Math.sqrt(mag)
+        for(let i=0;i<a.size;i++){
+            a.components[i] *= mag;
+        }
+    }
 }
