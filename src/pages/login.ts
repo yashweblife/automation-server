2fa6995c646e3aacdb500a8a475c8b75eb76cdfd
yashweblife <yashweblife20@gmail.com>
2023-05-12 01:44:20 -0500

    login.ts needs more work

---
+++ src/pages/login.ts
@@ -0,0 +1,14 @@
+const signup_form = document.querySelector('#signup-form')
+const login_form = document.querySelector('#login-form')
+
+const login_button = document.querySelector("#login-button")
+const signup_button = document.querySelector("#signup-button")
+
+const username_input = document.querySelector('#username-input')
+const password_input = document.querySelector('#password-input')
+
+const signup_toggle = document.querySelector('#signup-toggle-button')
+const login_toggle = document.querySelector('#login-toggle-button')
+
+function server_check_login() { }
+function server_check_signup() { }
