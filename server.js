3bd356a48db9661bf56d7107b2da4a964aec6528
yashweblife <yashweblife20@gmail.com>
2023-05-12 12:37:50 -0500

    add routes

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 import express from 'express';
 import path from 'path';
 const app = express();
-const port = 3001;
+const port = 3000;
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
 app.use(express.static(path.join(__dirname,'dist')))
 
 app.get('/',(req, res)=>{
     res.sendFile('index.html');
 })
+app.post('/handle_login',(req, res)=>{})
+app.post('/handle_signup',(req, res)=>{})
+app.post('/handle_logout',(req, res)=>{})
+
+app.get('/todo-lists', (req, res)=>{})
+app.post('/todo-list-add-list',(req,res)=>{})
+app.post('/todo-list-remove-list',(req,res)=>{})
+app.post('/todo-list-update-list',(req,res)=>{})
+app.post('/todo-add-item',(req,res)=>{})
+app.post('/todo-remove-item',(req,res)=>{})
+app.post('/todo-update-item',(req,res)=>{})
+
+app.get('/image_classifier', (req, res)=>{})
+
+app.get('/devices', (req, res)=>{})
+
+app.get('/compose', (req, res)=>{})
 
 app.listen(port,()=>{
     console.log("Hosting on ", port)
