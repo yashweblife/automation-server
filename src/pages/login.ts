27d1d01c01def2b045d0f22aa3c9ca20e80b3650
yashweblife <yashweblife20@gmail.com>
2023-05-13 03:23:08 -0500

    add auth frontend

--- src/pages/login.ts
+++ src/pages/login.ts
@@ -5,11 +5,46 @@ const login_form = document.querySelector('#login-form')
 const login_button = document.querySelector("#login-button")
 const signup_button = document.querySelector("#signup-button")
 
-const username_input = document.querySelector('#username-input')
-const password_input = document.querySelector('#password-input')
+const username_input: HTMLInputElement = document.querySelector('#username-input')!
+const password_input: HTMLInputElement = document.querySelector('#password-input')!
+
+const signup_username_input: HTMLInputElement = document.querySelector('#signup-username-input')!
+const signup_password_input: HTMLInputElement = document.querySelector('#signup-password-input')!
 
 const signup_toggle = document.querySelector('#signup-toggle-button')
 const login_toggle = document.querySelector('#login-toggle-button')
 
-function server_check_login() { }
-function server_check_signup() { }
+function server_check_login() {
+    const data = {
+        username: username_input.value,
+        password: password_input.value
+    }
+    fetch('/login', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }).then((res: Response) => {
+        if (res) {
+            console.log(res);
+        }
+    })
+}
+function server_check_signup() {
+    const data = {
+        username: signup_username_input.value,
+        password: signup_password_input.value
+    }
+    fetch('/singup', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }).then((res: Response) => {
+        if (res) {
+            console.log(res)
+        }
+    })
+}
