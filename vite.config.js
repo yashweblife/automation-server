a2b08a850b34fe5c7bd31b4db8a2bf8d7c2ed20d
yashweblife <yashweblife20@gmail.com>
2023-05-13 05:29:20 -0500

    add end files

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from "vite"
 export default defineConfig({
   server:{
     port:3000
-  }  
+  },
+  build:{
+    rollupOptions:{
+      input:{
+        index:'index.html',
+        home:'home.html',
+        todolist:'todolist.html'
+      }
+    }
+  }
 })
