4252f8b155786882d9fc3bb3bc59566e7d2ccf33
yashweblife <yashweblife20@gmail.com>
2023-05-13 03:26:35 -0500

    add event binding

--- src/pages/login.ts
+++ src/pages/login.ts
@@ -1,9 +1,9 @@
 import "../styles/login.scss"
-const signup_form = document.querySelector('#signup-form')
-const login_form = document.querySelector('#login-form')
+const signup_form: HTMLElement = document.querySelector('#signup-form')!
+const login_form: HTMLElement = document.querySelector('#login-form')!
 
-const login_button = document.querySelector("#login-button")
-const signup_button = document.querySelector("#signup-button")
+const login_button: HTMLButtonElement = document.querySelector("#login-button")!
+const signup_button: HTMLButtonElement = document.querySelector("#signup-button")!
 
 const username_input: HTMLInputElement = document.querySelector('#username-input')!
 const password_input: HTMLInputElement = document.querySelector('#password-input')!
@@ -11,8 +11,8 @@ const password_input: HTMLInputElement = document.querySelector('#password-input
 const signup_username_input: HTMLInputElement = document.querySelector('#signup-username-input')!
 const signup_password_input: HTMLInputElement = document.querySelector('#signup-password-input')!
 
-const signup_toggle = document.querySelector('#signup-toggle-button')
-const login_toggle = document.querySelector('#login-toggle-button')
+const signup_toggle: HTMLButtonElement = document.querySelector('#signup-toggle-button')!
+const login_toggle: HTMLButtonElement = document.querySelector('#login-toggle-button')!
 
 function server_check_login() {
     const data = {
@@ -48,3 +48,18 @@ function server_check_signup() {
         }
     })
 }
+
+signup_toggle.addEventListener('click', () => {
+    login_form.classList.add('display-off')
+    signup_form.classList.remove('display-off')
+})
+login_toggle.addEventListener('click', () => {
+    signup_form.classList.add('display-off')
+    login_form.classList.remove('display-off')
+})
+login_button.addEventListener('click', () => {
+    server_check_login();
+})
+signup_button.addEventListener('click', () => {
+    server_check_signup();
+})
