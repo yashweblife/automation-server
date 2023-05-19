3bf475077736debd1adde8f5a25442c5f9436b6a
yashweblife <yashweblife20@gmail.com>
2023-05-19 16:59:06 -0500

    new awesome create functions

--- src/pages/todolist.ts
+++ src/pages/todolist.ts
@@ -19,33 +19,64 @@ if (get_current_user() == "undefined") {
     window.open('./index.html', "_self");
 }
 
-export function create_todolist_dom(list:TodoListInterface){
-    const dom = $.makeDOM("list")
-    
-    return(dom)
+export function create_todolist_dom(list: TodoListInterface) {
+    const dom = new $("details")
+    const listData = list.list.map((val: TodoListItemInterface) => create_todolist_item_dom(val))
+    dom.addChildren([
+        new $("summary").addChild(
+            new $().addClass("info")
+                .addChildren(
+                    [
+                        new $().addClass("content")
+                            .addChildren([
+                                new $('h4').setText(list.name),
+                                new $('nav').addChild(
+                                    new $('button').setText('Delete')
+                                ),
+                            ])
+                        ,
+                        new $().addClass("footer")
+                            .addChildren([
+                                new $('p').setText(list.status),
+                                new $('p').setText(list.date)
+                            ]),
+                    ]
+                )
+        ),
+        new $().addClass("list").addChildren(listData)
+    ])
+    return (dom)
 }
 
 export function create_todolist_item_dom(data: TodoListItemInterface) {
-    const dom = $.makeDOM().addClass("item").addChildren([
-        $.makeDOM()
-            .addClass("header")
-            .addChildren([
-                $.makeDOM("h5")
-                    .setText(data.name),
-
-                $.makeDOM("nav")
-                    .addChildren([
-                        $.makeDOM("button")
-                            .setText("Mark"),
-
-                        $.makeDOM("button")
-                            .setText("Close")
-                    ]),
-            ]),
-        $.makeDOM().addClass("content").addChildren([
-            $.makeDOM("code").setText(data.date),
-            $.makeDOM("p").setText(data.info)
+    const handle_mark_button = () => { }
+    const handle_close_button = () => { }
+    const item = new $()
+        .addClass("item")
+        .addChildren([
+            new $()
+                .addClass("header")
+                .addChildren([
+                    new $("h5")
+                        .setText(data.name),
+                    new $("nav")
+                        .addChildren([
+                            new $("button")
+                                .setText("Mark")
+                                .addEvent("click", handle_mark_button),
+                            new $("button")
+                                .setText("Close")
+                                .addEvent("click", handle_close_button)
+                        ])
+                ]),
+            new $()
+                .addClass("content")
+                .addChildren([
+                    new $("code")
+                        .setText(data.date),
+                    new $("p")
+                        .setText(data.info)
+                ]),
         ])
-    ])
-    return (dom)
+    return (item);
 }
