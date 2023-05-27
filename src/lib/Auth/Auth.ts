3b7b1e921fae678aca5c378b9296a085650573bd
yashweblife <yashweblife20@gmail.com>
2023-05-26 21:51:06 -0500

    add auth class

---
+++ src/lib/Auth/Auth.ts
@@ -0,0 +1,31 @@
+export class Auth{
+    constructor(){
+
+    }
+    public checkAuthState(){
+
+    }
+    public loginUser(){
+
+    }
+    public createUser(){
+
+    }
+    public logoutUser(){
+
+    }
+    public deleteUser(){
+
+    }
+    public updateUserDetails(){
+
+    }
+    
+}
+
+export function checkAuthState(){}
+export function loginUser(){}
+export function createUser(){}
+export function logoutUser(){}
+export function deleteUser(){}
+export function updateUserDetails(){}
